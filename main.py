214a6f294da5bacb7601f1f563490f139a0c43da
davfranco1 <david@francoonline.net>
2025-02-02 12:40:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -348,7 +348,7 @@ st.markdown("""
 
 
 if st.session_state.page == "Datos de compra y financiación":
-    st.markdown('<p style="color: #224094; font-size: 18px;">• Introduce los datos correspondientes a la compra y la financiación, seguido del botón <strong>Ver resultados</strong>. Puedes obtener más información deslizando sobre cada valor deslizando sobre la ❓</p>', unsafe_allow_html=True)
+    st.markdown('<p style="color: #224094; font-size: 18px;">• Introduce los datos correspondientes a la compra y la financiación, seguido del botón <strong>Ver resultados</strong>. <br>• Puedes obtener más información deslizando sobre cada valor deslizando sobre la ❓</p>', unsafe_allow_html=True)
 
     # Create two columns
     col1, col2 = st.columns(2)
