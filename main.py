f713b025f907ea23d613b2730d40232ec5dea45e
davfranco1 <david@francoonline.net>
2025-02-02 14:12:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ st.markdown("<hr>", unsafe_allow_html=True)
 
 if st.session_state.page == "Datos de compra y financiación":
     st.markdown("""<p style="color: #224094; font-size: 18px;">• Introduce los datos correspondientes a la compra y la financiación, seguido del botón <strong>Ver resultados</strong>. <br>• Puedes obtener más información deslizando sobre cada valor deslizando sobre la ❓.</p>
-    <p style="color: #224094; font-size: 14px;">• Los resultados son estimaciones, y nunca deben considerarse consejos de inversión. Antes de invertir, asegúrese de <strong>consultar con un experto</strong>.</p>""",
+    <p style="color: #224094; font-size: 14px;">‣ Los resultados son estimaciones, y nunca deben considerarse consejos de inversión. Antes de invertir, asegúrese de <strong>consultar con un experto</strong>.</p>""",
       unsafe_allow_html=True)
 
     # Create two columns
